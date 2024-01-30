27b6f5dd77c47e99254ce88072e26be4d2000f47
xXQiuChenXx <lautaihong2004@gmail.com>
2024-01-30 23:53:40 +0800

    Final

---
+++ JokeWebApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public required string JokeQuestion { get; set; }
+        public required string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+
+    }
+}
